da3bc52ada15dde12b1867b6817afa59fe4e09c5
Wilk <ma.wilk@tlen.pl>
2014-01-20 22:46:03 +0100

    peekaboo

--- input.erl
+++ input.erl
@@ -1,5 +1,5 @@
 -module(input).
--export([add_chars/0,add_party/0]).
+-export([add_chars/0,add_party/0,add_enemies1/0,add_enemies2/0,add_to_party/0]).
 
 add_chars() ->
 	start:add_character("Minsc","Ranger"),
@@ -15,11 +15,21 @@ add_chars() ->
 
 add_party() ->
 	start:add_party("Fast and Furious"),
-	start:add_party("DragonBall Team").
+	start:add_party("Freelancer").
 	
-add_enemies() ->
+add_to_party() ->
+	start:add_to_party("Minsc","Fast and Furious"),
+	start:add_to_party("Edwin","Fast and Furious"),
+	start:add_to_party("Imoen","Fast and Furious"),
+	start:add_to_party("Dyanheir","Fast and Furious").
+	
+add_enemies1() ->
 	start:add_enemy("Greater Wyvern", 100, "Scares small children in nearby village."),
 	start:add_enemy("Doppleganger", 250, "Leads pack of ghouls and ettercaps."),
 	start:add_enemy("Basilisk", 500, "What an ugly monster!"),
 	start:add_enemy("Smaug", 1000, "Destroys cities in his free time.").
 	
+add_enemies2() ->
+	start:add_enemy("Small Wyvern", 50, "Steals food from fridges."),
+	start:add_enemy("Ghoul", 150, "Nobody likes him since he plays peekaboo on city's graveyard."),
+	start:add_enemy("Kitty", 700, "Fearsome beast.").
--- start.erl
+++ start.erl
@@ -1,12 +1,12 @@
 -module(start).
 -export([install/1,init_tables/1]).
--export([add_character/2, add_party/1, add_to_party/2, find_by_spec/1]).
+-export([add_character/2, add_party/1, add_to_party/2, find_by_spec/1,
+		 kill_enemy/2, pay/2, add_enemy/3, find_prize/1]).
 
 %% 
 
 -record(character, {name,
 				    specialisation,
-				    money,
 				    left_hand,
 				    right_hand,
 				    armor}).
@@ -14,14 +14,12 @@
 -record(join_party, {party_name,
 					 charr_name}).
 				   
--record(party, {party_name}).
+-record(party, {party_name,
+				money}).
 
 -record(enemy, {boss_name,
 				prize,
 				event_description}).
-				
--record(join_event, {party_name,
-					 boss_name}).
 				
 -record(item, {item_name,
 			   item_type,
@@ -48,33 +46,30 @@ init_tables(Nodes) ->
 						
     mnesia:create_table(join_party,
                         [{attributes, record_info(fields, join_party)},
+						{type,bag},
 						{disc_copies, Nodes}]),
 						
     mnesia:create_table(enemy,
                         [{attributes, record_info(fields, enemy)},
 						{disc_copies, Nodes},
-						{local_content, true}]),
-						
-	mnesia:create_table(join_event,
-                        [{type, bag}, 
-						{attributes, record_info(fields, join_event)},
-						{disc_copies, Nodes},
 						{local_content, true}]).
+	
 	
 add_character(Name, Specialisation) ->
 	F = fun() ->
 		mnesia:write(#character{name = Name,
 							    specialisation = Specialisation,
-							    money = 0,
 							    left_hand = "Empty",
 							    right_hand = "Empty",
 							    armor = "None"})
 	end,
 	mnesia:activity(transaction, F).
 	
+	
 add_party(Name) ->
 	F = fun() ->
-		mnesia:write(#party{party_name = Name})
+		mnesia:write(#party{party_name = Name,
+							money = 0})
 	end,
 	mnesia:activity(transaction, F).
 
@@ -92,6 +87,7 @@ add_to_party(Name,Party) ->
 	end,
 	mnesia:activity(transaction,F).	
 	
+	
 add_enemy(BossName, Prize, Desc) ->
 	F = fun() ->
 		mnesia:write(#enemy{boss_name = BossName,
@@ -99,20 +95,6 @@ add_enemy(BossName, Prize, Desc) ->
 							event_description = Desc})
 	end,
 	mnesia:activity(transaction, F).
-
-
-add_party_to_event(Name,BossNaame) ->
-	F = fun() ->
-		case mnesia:read({party, Name}) =:= [] orelse
-		     mnesia:read({enemy, BossName}) =:= [] of
-		true ->
-			{error, unknown_argument};
-		false ->
-			mnesia:write(#join_event{party_name = Name,
-									 boss_name = BossName})
-		end
-	end,
-	mnesia:activity(transaction,F).	
 
 	
 find_by_spec(Specialisation) ->
@@ -124,6 +106,28 @@ find_by_spec(Specialisation) ->
 	mnesia:activity(transaction, F).
 	
 	
+kill_enemy(Party, Name) ->
+	pay(Party, find_prize(Name)),
+	F = fun() -> mnesia:delete({enemy, Name}) end,
+	mnesia:activity(transaction, F).
+	
+	
+find_prize(Name) ->
+	F = fun() -> mnesia:read({enemy, Name}) end,
+	case mnesia:activity(transaction, F) of
+		[] -> 0;
+		[#enemy{prize = Money}] -> Money
+	end.
+	
+	
+pay(Party, Prize) ->
+    F = fun() ->
+        [P] = mnesia:read({party, Party}),
+        Money = P#party.money,
+        New = P#party{money = Money + Prize},
+        mnesia:write(New)
+    end,
+    mnesia:transaction(F).
 	
 	
 	
